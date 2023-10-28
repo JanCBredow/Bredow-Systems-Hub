093c456f74c95cf8e33fd9376be500741bdef2bb
Jan Christopher Bredow <jan@bredow.systems>
2023-10-29 01:40:48 +0200

    testing

--- web/js/app.js
+++ web/js/app.js
@@ -7,55 +7,21 @@ function openURL(url) {
 }
 
 setInterval(function () {
-    updateStatus('URL_für_Statistik_Container_1', 'stat-container-1-status');
-    updateStatus('URL_für_Statistik_Container_2', 'stat-container-2-status');
+    checkPortReachability('vps.jan-bredow.de', 22, 'stat-container-1-status');
+    checkPortReachability('bredow.systems', 80, 'stat-container-2-status');
 }, 5000);
 
-// Host und Port festcodieren
-var statContainer1Host = 'vps.jan-bredow.de';
-var statContainer1Port = 22;
-var statContainer2Host = 'bredow.systems';
-var statContainer2Port = 80;
-
-function pingServer(host, port, timeout, callback) {
-    var start = new Date();
-    var response = {
-        status: 'OFFLINE',
-        ping: null
+function checkPortReachability(host, port, elementId) {
+    var img = new Image();
+    img.src = '//' + host + ':' + port;
+    img.onload = function() {
+        updateStatus(elementId, 'ONLINE');
     };
-
-    var socket = new WebSocket('wss://' + host + ':' + port);
-
-    socket.onopen = function () {
-        response.status = 'ONLINE';
-        var end = new Date();
-        response.ping = end - start;
-        socket.close();
-        callback(response);
+    img.onerror = function() {
+        updateStatus(elementId, 'OFFLINE');
     };
-
-    socket.onclose = function () {
-        if (response.status === 'OFFLINE') {
-            callback(response);
-        }
-    };
-
-    setTimeout(function () {
-        if (response.status === 'OFFLINE') {
-            socket.close();
-        }
-    }, timeout);
 }
 
-pingServer(statContainer1Host, statContainer1Port, 5000, function (result) {
-    updateStatus('URL_für_Statistik_Container_1', 'stat-container-1-status', result);
-});
-
-pingServer(statContainer2Host, statContainer2Port, 5000, function (result) {
-    updateStatus('URL_für_Statistik_Container_2', 'stat-container-2-status', result);
-});
-
-function updateStatus(url, elementId, result) {
-    document.getElementById(elementId).textContent = result.status === 'ONLINE' ?
-        'ONLINE (' + result.ping + 'ms)' : 'OFFLINE';
+function updateStatus(elementId, status) {
+    document.getElementById(elementId).textContent = status;
 }
