9e34e9003cab0e28e1a8de486a01c4fbb1d11d6b
Jan Christopher Bredow <jan@bredow.systems>
2023-10-29 01:21:58 +0200

    testing

--- web/js/app.js
+++ web/js/app.js
@@ -11,6 +11,51 @@ setInterval(function () {
     updateStatus('URL_für_Statistik_Container_2', 'stat-container-2-status');
 }, 5000);
 
-function updateStatus(url, elementId) {
-    document.getElementById(elementId).textContent = 'ONLINE (' + Math.floor(Math.random() * 100) + 'ms)';
+// Host und Port festcodieren
+var statContainer1Host = 'vps.jan-bredow.de';
+var statContainer1Port = 22;
+var statContainer2Host = 'bredow.systems';
+var statContainer2Port = 80;
+
+function pingServer(host, port, timeout, callback) {
+    var start = new Date();
+    var response = {
+        status: 'OFFLINE',
+        ping: null
+    };
+
+    var socket = new WebSocket('ws://' + host + ':' + port);
+
+    socket.onopen = function () {
+        response.status = 'ONLINE';
+        var end = new Date();
+        response.ping = end - start;
+        socket.close();
+        callback(response);
+    };
+
+    socket.onclose = function () {
+        if (response.status === 'OFFLINE') {
+            callback(response);
+        }
+    };
+
+    setTimeout(function () {
+        if (response.status === 'OFFLINE') {
+            socket.close();
+        }
+    }, timeout);
+}
+
+pingServer(statContainer1Host, statContainer1Port, 5000, function (result) {
+    updateStatus('URL_für_Statistik_Container_1', 'stat-container-1-status', result);
+});
+
+pingServer(statContainer2Host, statContainer2Port, 5000, function (result) {
+    updateStatus('URL_für_Statistik_Container_2', 'stat-container-2-status', result);
+});
+
+function updateStatus(url, elementId, result) {
+    document.getElementById(elementId).textContent = result.status === 'ONLINE' ?
+        'ONLINE (' + result.ping + 'ms)' : 'OFFLINE';
 }
